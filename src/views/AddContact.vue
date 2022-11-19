db9626a46de7b384b8280d57857bc70f54aba3ae
Tudor Telechi <tudor.tki@gmail.com>
2022-11-19 22:15:42 +0200

    Finished ContactManager and AddContact

--- src/views/AddContact.vue
+++ src/views/AddContact.vue
@@ -1,5 +1,52 @@
 <template>
-  <h2>Add aici</h2>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col">
+        <p class="h3 text-success fw-bold">Add Contact</p>
+        <p class="fst-italic">
+          Una dintre aceste caracteristici simple dar utile este copierea și
+          lipirea oricărui text în documentele dvs. Word. Când doriți să citați
+          ceva sau aveți nevoie de cuvinte duplicate în fișierul dvs., puteți
+          copia și lipi pur și simplu din sursa originală. Acest lucru vă
+          accelerează munca în mai multe moduri, deoarece n
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col-md-4">
+        <form>
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Name" />
+          </div>
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Photo URL" />
+          </div>
+          <div class="mb-2">
+            <input type="Email" class="form-control" placeholder="Email" />
+          </div>
+          <div class="mb-2">
+            <input type="number" class="form-control" placeholder="Mobile" />
+          </div>
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Company" />
+          </div>
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Title" />
+          </div>
+          <div class="mb-2">
+            <select class="form-control">
+              <option value="">Select Group</option>
+            </select>
+          </div>
+          <div class="mb-2">
+            <input type="submit" class="btn btn-success" value="Create" />
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
--- src/views/ContactManager.vue
+++ src/views/ContactManager.vue
@@ -35,6 +35,53 @@
       </div>
     </div>
   </div>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card my-2 list-group-item-success shadow-lg">
+          <div class="card-body">
+            <div class="row align-items-center">
+              <div class="col-sm-4">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtF3o2PvqxOMHgdrpj_YRItsLBjxyTeNZu_Q&usqp=CAU"
+                  alt=""
+                  class="contact-img" />
+              </div>
+              <div class="col-sm-7">
+                <ul class="list-group">
+                  <li class="list-group-item">
+                    Name: <span class="fw-bold">Name</span>
+                  </li>
+                  <li class="list-group-item">
+                    Email: <span class="fw-bold">Email</span>
+                  </li>
+                  <li class="list-group-item">
+                    Mobile: <span class="fw-bold">Mobile</span>
+                  </li>
+                </ul>
+              </div>
+              <div
+                class="col-sm-1 d-flex flex-column justify-content-center align-items-center">
+                <RouterLink
+                  to="/contacts/view/:contactId"
+                  class="btn btn-warning my-1">
+                  <i class="fa fa-eye"></i>
+                </RouterLink>
+                <RouterLink
+                  to="/contacts/edit/:contactId"
+                  class="btn btn-primary my-1">
+                  <i class="fa fa-pen"></i>
+                </RouterLink>
+                <button class="btn btn-danger my-1">
+                  <i class="fa fa-trash"> </i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
